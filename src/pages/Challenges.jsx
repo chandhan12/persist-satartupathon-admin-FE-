eee5c414100d7534dd0662ae9edab3e938668f5f
chandhan <chandhan.g0401@gmail.com>
2025-02-27 23:43:18 +0530

    added data fetching functionality

--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { plusIcon } from '../icons'
+import axios from 'axios'
 
 
 
 
 const Challenges = () => {
+
+  const [challenges,setChallenges]=useState()
+
+
+  useEffect(()=>{
+    const fetchChallenges=async ()=>{
+      const result=await axios.get("http://localhost:3000/api/admin/challenges")
+      setChallenges(result.data)
+    }
+
+    fetchChallenges()
+
+  },[])
+
+  console.log(challenges)
+
   return (
     <div className='h-screen w-full bg-red-200'>
     <div className='flex justify-between m-2 '>
@@ -16,11 +33,25 @@ const Challenges = () => {
         <button className='cursor-pointer'>ADD CHALLENGE</button>
       </div>
     </div>
-    <div className='m-2 h-auto w-full bg-white'>
-      <div>
-        hi
-      </div>
+    <div className='m-2 grid grid-cols-10 items-center  h-14  bg-white'>
+      
+       <div className='flex gap-10 col-span-2 p-1 bg-lime-200'>
+        <div>Sl No.</div>
+        <div>Title</div>
+       </div>
+       
+       <div className='flex gap-10 col-span-5 bg-blue-200'>
+        <p>Funding</p>
+        <p>Deadline</p>
+        <p>Description</p>
+       </div>
+        <div className='flex gap-10 col-span-3 bg-slate-300'>
+        <p>Review Videos</p>
+        <p>Challenge Videos</p>
+        <p>Status</p>
+       </div> 
     </div>
+    
     </div>
   )
 }
